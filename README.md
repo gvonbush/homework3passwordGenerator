75b29acd50eb2da9df40a3e97c4d7fdaa9e4af54
geeveebee <gvonbush@me.com>
2019-11-09 22:43:21 -0500

    I can't generate the buttons. I will keep trying. I'm slow.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# homework3passwordGenerator
+# # Password Generator
+This repository "password-generator" creates passwords and was designed using html, css and javascript. This application allows the user to choose a password length from 8-128 and then gives the user the option to use special charaters. It is important to know that this password generator automatically uses lowercase and uppercase letters as well as numbers.
+
+# Access and Preview of Repository
+
+https://gvonbush.github.io/homework3passwordGenerator/.
+
+
+# Credits
+Johns Hopkins full-Stack Bootcamp
+
+Stetson Lewis
+
+Donald Hesler
+
+# License
+MIT License
+
+Copyright (c) 2019 pmitch242
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-function passwordGenerator (){
+function passwordGenerator() {
     var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
     var password = ""
 
+    var passwordLength = ""
+
     var specialCharacter = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^",
-    "~", "*", "?", ":"]
-var randomNumber = Math.floor(Math.random() * (120) + 8);
-  console.log(randomNumber)
-    for (i=0; i<randomNumber; i++){
+        "~", "*", "?", ":"]
+    
+    while (passwordLength == ""){
+      debugger
+      var newLength =  parseInt (prompt ("how long is the password? (between 8 and 128?)"))
+      if (newLength >= 8 && newLength <=128){
+          passwordLength = newLength
+
+      }
+    }
+    var randomNumber = Math.floor(Math.random() * (120) + 8);
+    console.log(randomNumber)
+    for (i = 0; i < passwordLength; i++) {
         var randomAlphabet = alphabet[Math.floor(Math.random() * alphabet.length)]
         console.log(randomAlphabet)
-        var randomSpecialCharacter = specialCharacter[Math.floor(Math.random() * specialCharacter.length)] 
+        var randomSpecialCharacter = specialCharacter[Math.floor(Math.random() * specialCharacter.length)]
         var randomInteger = Math.floor(Math.random() * Math.floor(9))
-        console.log (randomSpecialCharacter, randomInteger)
+        console.log(randomSpecialCharacter, randomInteger)
     }
 }
-passwordGenerator ()
+document.getElementById("generate").addEventListener("click",passwordGenerator);
+//passwordGenerator()
+
+    }
+}
+debugger
+//call a prompt for if else
